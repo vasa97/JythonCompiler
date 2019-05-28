4dd4300b5cf5560e92c514efa490efe441035297
mohammadHossein <mhk9239@gmail.com>
2019-05-27 18:56:35 -0700

    some change

--- src/gen/jythonBaseListener.java
+++ src/gen/jythonBaseListener.java
@@ -5,6 +5,9 @@ import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class provides an empty implementation of {@link jythonListener},
  * which can be extended to create a listener which only needs to handle a subset
@@ -55,7 +58,7 @@ public class jythonBaseListener implements jythonListener {
 	 */
 	@Override public void exitClassDec(jythonParser.ClassDecContext ctx) {
 
-		if( current.get("main", Kind.METHOD) == null ) System.out.println("error");
+		if( current.get("main", Kind.METHOD) == null ) System.out.println("error main method");
 	}
 	/**
 	 * {@inheritDoc}
@@ -75,14 +78,13 @@ public class jythonBaseListener implements jythonListener {
 	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void enterVarDec(jythonParser.VarDecContext ctx) {
+			Symbol s = new VariableSymbol(ctx.type().getText(), ctx.ID().getText());
 
-		Symbol s = new VariableSymbol(ctx.type().getText(), ctx.ID().getText());
-
-		if (current.lookup(s, Kind.ATTRIBUTE) == true) System.out.println("error");
-		else {
-			current.insertVariable(ctx.type().getText(), ctx.ID().getText(), Kind.ATTRIBUTE);
-			System.out.println(s.getId()+": added to table");
-		}
+			if (current.lookup(s, Kind.ATTRIBUTE) == true) System.out.println("error");
+			else {
+				current.insertVariable(ctx.type().getText(), ctx.ID().getText(), Kind.ATTRIBUTE);
+				System.out.println(s.getId() + ": added to table");
+			}
 	}
 	/**
 	 * {@inheritDoc}
@@ -109,6 +111,14 @@ public class jythonBaseListener implements jythonListener {
 	 */
 	@Override public void enterMethodDec(jythonParser.MethodDecContext ctx) {
 
+		if (ctx.type() == null) current.insertMethod(Type.VOID, ctx.ID().getText(), ctx.parameters(), false);
+		if (ctx.type() != null) {
+			Type dayEnum = Type.valueOf(ctx.type().getText().toUpperCase());
+			Symbol s = new MethodSymbol(dayEnum, ctx.ID().getText(), ctx.parameters(), false);
+			if (current.lookup(s, Kind.METHOD) == true) System.out.println("error method");
+
+			current.insertMethod(dayEnum, ctx.ID().getText(), ctx.parameters(), false);
+		}
 	}
 	/**
 	 * {@inheritDoc}
